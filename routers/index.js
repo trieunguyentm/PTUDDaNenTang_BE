fa8e2248d2b56f95c1c65f82ca22e621d82191ef
trieunguyentm <trieunguyen241102@gmail.com>
2023-11-02 19:29:28 +0700

    rename router

--- routers/index.js
+++ routers/index.js
@@ -1,8 +1,8 @@
 import { Router } from "express"
-import authRouter from "./userRouter.js"
+import userRouter from "./userRouter.js"
 
 const router = Router()
 
-router.use("/auth", authRouter)
+router.use("/auth", userRouter)
 
 export default router
