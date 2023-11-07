fc819dafd97cd9f1f2034dab1b57f32f7b44183a
trieunguyentm <trieunguyen241102@gmail.com>
2023-11-07 10:04:48 +0700

    update: done API uploadAvatar user

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,6 +1,8 @@
 import admin from "../firebase/connect.js"
 import jwt from "jsonwebtoken"
 import dotenv from "dotenv"
+import { json } from "express"
+import { getUser, updateUser } from "../firebase/userService.js"
 
 dotenv.config({ path: "../.env.development" })
 /** Tạo storage bucket */
@@ -11,7 +13,7 @@ export const uploadFile = async (req, res) => {
   const token = req.header("Authorization").split(" ")[1]
   let username, gmail
   if (!token)
-    return res.status(401).json({ msg: "Chưa cung cấp token", code: 5 })
+    return res.status(401).json({ msg: "Chưa cung cấp token", code: 1 })
   try {
     const decoded = jwt.verify(token, process.env.KEY_JWT)
     username = decoded.username
@@ -20,13 +22,13 @@ export const uploadFile = async (req, res) => {
     console.log("Xảy ra lỗi khi phân tích token: ", error)
     return res
       .status(500)
-      .json({ msg: "Xảy ra lỗi khi phân tích token", code: 1 })
+      .json({ msg: "Xảy ra lỗi khi phân tích token", code: 2 })
   }
 
   if (!token || !file) {
     return res
       .status(400)
-      .json({ msg: "Thiếu thông tin người dùng hoặc tệp", code: 2 })
+      .json({ msg: "Thiếu thông tin người dùng hoặc tệp", code: 3 })
   }
   /** Đường dẫn trên Firebase Storage */
   const remoteFilePath = `${username}/${file.originalname}`
@@ -40,7 +42,7 @@ export const uploadFile = async (req, res) => {
 
   blobStream.on("error", (error) => {
     console.error("Lỗi khi tải lên tệp:", error)
-    res.status(500).json({ message: "Lỗi khi tải lên tệp", code: 3 })
+    res.status(500).json({ msg: "Lỗi khi tải lên tệp", code: 4 })
   })
 
   blobStream.on("finish", () => {
@@ -50,18 +52,32 @@ export const uploadFile = async (req, res) => {
         action: "read",
         expires: "01-01-2030",
       },
-      (err, url) => {
+      async (err, url) => {
         if (err) {
           console.error("Lỗi khi lấy URL tải xuống:", err)
-          res
-            .status(500)
-            .json({ message: "Lỗi khi lấy URL tải xuống", code: 4 })
+          res.status(500).json({ msg: "Lỗi khi lấy URL tải xuống", code: 5 })
         } else {
           console.log("Tệp đã được tải lên thành công.")
           console.log("URL của tệp đã tải lên:", url)
-          res
-            .status(200)
-            .json({ message: "Tệp đã được tải lên thành công", url, code: 0 })
+          /** Sau khi có url của ảnh, lấy url để làm avatar cho người dùng */
+          try {
+            /** Update thông tin user */
+            await updateUser(username, { urlAvatar: url })
+            /** Lấy dữ liệu người dùng */
+            const userData = await getUser(username)
+            delete userData.password
+            return res.status(200).json({
+              msg: "Tệp đã được tải lên thành công",
+              code: 0,
+              userData,
+            })
+          } catch (error) {
+            console.log("Xảy ra lỗi khi cập nhật avatar người dùng")
+            json.status(500).json({
+              msg: "Xảy ra lỗi khi cập nhật avatar người dùng",
+              code: 6,
+            })
+          }
         }
       },
     )
--- middlewares/fileMiddleware.js
+++ middlewares/fileMiddleware.js
@@ -1,17 +1,28 @@
 import dotenv from "dotenv"
+import mime from "mime-types"
 
 dotenv.config({ path: "../.env.development" })
 
 export const checkUploadAvatar = (req, res, next) => {
   const file = req.file
   const authHeader = req.header("Authorization")
+
   if (!authHeader) {
     return res
       .status(401)
       .json({ msg: "Chưa cung cấp Authorization trong Header" })
   }
+
   if (!file) {
     return res.status(400).json({ msg: "Chưa cung cấp file" })
   }
+
+  /** Kiểm tra loại nội dung của tệp */
+  const contentType = mime.lookup(file.originalname)
+
+  if (!contentType || !contentType.startsWith("image/")) {
+    return res.status(400).json({ msg: "Tệp không phải là ảnh" })
+  }
+
   next()
 }
